73f8fa4f38f0f7af685ee1786278d90df892afd5
crisp-coder <crisp-coder@proton.me>
2026-03-11 07:47:35 -0500

    cleaned up pycache and line.py

--- line.py
+++
@@ -1,9 +0,0 @@
-
-class Line:
-    def __init__(p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-
-    def draw(self, canvas, color):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=color, width=2)
-
